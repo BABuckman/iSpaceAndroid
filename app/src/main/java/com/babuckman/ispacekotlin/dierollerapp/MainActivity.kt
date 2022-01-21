aa34cb079bdeb598e748aaae5680dfe3b8e954f0
Bernard <lordgiver@hotmail.com>
2022-01-21 08:47:22 +0000

    DieRoller App
    Android Project with Kotlin to demonstrate changing views

---
+++ app/src/main/java/com/babuckman/ispacekotlin/dierollerapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.babuckman.ispacekotlin.dierollerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
