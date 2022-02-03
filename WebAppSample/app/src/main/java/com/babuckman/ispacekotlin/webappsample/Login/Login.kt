e95c2978e240890463adb1005e89fd472b67e36b
Bernard <lordgiver@hotmail.com>
2022-02-03 12:30:56 +0000

    Login App
    Demo for login app

---
+++ WebAppSample/app/src/main/java/com/babuckman/ispacekotlin/webappsample/Login/Login.kt
@@ -0,0 +1,12 @@
+package com.babuckman.ispacekotlin.webappsample.Login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.babuckman.ispacekotlin.webappsample.R
+
+class Login:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
