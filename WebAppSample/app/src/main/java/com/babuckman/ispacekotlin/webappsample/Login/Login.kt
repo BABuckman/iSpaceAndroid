74a63754300cdfd8545390ed334b12702d90305c
Bernard <lordgiver@hotmail.com>
2022-02-03 14:09:31 +0000

    Update to WebApp

--- WebAppSample/app/src/main/java/com/babuckman/ispacekotlin/webappsample/Login/Login.kt
+++ WebAppSample/app/src/main/java/com/babuckman/ispacekotlin/webappsample/Login/Login.kt
@@ -1,12 +1,37 @@
 package com.babuckman.ispacekotlin.webappsample.Login
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.babuckman.ispacekotlin.webappsample.R
 
 class Login:AppCompatActivity() {
+    private lateinit var username:EditText
+    private lateinit var password: EditText
+    private lateinit var login:Button
+    private lateinit var cancel:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        login = findViewById(R.id.login)
+        cancel = findViewById(R.id.Cancel)
+        //Write implementation for login
+        login.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+        })
+        //write implementation for cancel
+        cancel.setOnClickListener(object: View.OnClickListener{
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+        })
     }
+
 }
