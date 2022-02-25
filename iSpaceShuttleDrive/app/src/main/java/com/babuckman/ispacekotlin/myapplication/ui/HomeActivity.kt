09de3ceae9036531e54182ab8c66154703398b23
Bernard <lordgiver@hotmail.com>
2022-02-25 22:09:30 +0000

    ShuttleDrive Update
    Update to the following components:
    1. navigation drawer
    2. recyclerview
    3. adapter (to be fully implemented)

--- iSpaceShuttleDrive/app/src/main/java/com/babuckman/ispacekotlin/myapplication/ui/HomeActivity.kt
+++ iSpaceShuttleDrive/app/src/main/java/com/babuckman/ispacekotlin/myapplication/ui/HomeActivity.kt
@@ -2,22 +2,72 @@ package com.babuckman.ispacekotlin.myapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.babuckman.ispacekotlin.myapplication.R
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
     //instantiate variables
-    lateinit var toggle: ActionBarDrawerToggle
-    lateinit var toolbar: Toolbar
+    lateinit var toggle:ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+    lateinit var toolbar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-
         //setup variables
         toolbar = findViewById(R.id.toolbar)
-       // toggle = ActionBarDrawerToggle(this,toolbar,R.string.openDrawer, R.string.closeDrawer )
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+
+        //this makes the menu icon on the toolbar clickable
+       toggle = ActionBarDrawerToggle(this,drawerLayout, toolbar,R.string.openDrawer, R.string.closeDrawer )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //Set navigation listener
+        navigationView.setNavigationItemSelectedListener { data->
+            when(data.itemId){
+                R.id.Booking -> goToBooking()
+                R.id.Invoice -> goToInvoice()
+                R.id.History -> goToHistory()
+                R.id.Feedback -> goToFeedback()
+                R.id.Rateus -> goToRate()
+                R.id.Logout -> goToLogout()
+                else -> false
+            }
+        }
+    }
+
+    private fun goToLogout():Boolean {
+        return true
+    }
+
+    private fun goToRate():Boolean{
+        return true
+    }
+
+    private fun goToFeedback():Boolean {
+        return true
+    }
+
+    private fun goToHistory(): Boolean {
+        return true
+    }
+
+    private fun goToInvoice(): Boolean {
+        return true
+    }
+
+    private fun goToBooking(): Boolean {
+        Toast.makeText(this@HomeActivity, "Success!", Toast.LENGTH_SHORT).show()
+        return true
     }
 }
